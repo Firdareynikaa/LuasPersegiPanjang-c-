84478373fa0d9d2f506023832951bb6574fa1b0a
Firdareynikaa <firda_reinika_26rpl@student.smktelkom-mlg.sch.id>
2019-07-22 14:26:23 +0700

    selesai

---
+++ LUAS PERSEGI PANJANG 01/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LUAS_PERSEGI_PANJANG_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int PANJANG = 7;
+            int LEBAR   = 5;
+            int LUAS;
+            Console.WriteLine("LUAS PERSEGI PANJANG");
+            Console.WriteLine();
+            Console.WriteLine("PANJANG =" + PANJANG);
+            Console.WriteLine("LEBAR   =" + LEBAR);
+            LUAS = PANJANG * LEBAR;
+            Console.WriteLine("LUAS    =" + LUAS);
+            Console.ReadKey();
+        }
+    }
+}
